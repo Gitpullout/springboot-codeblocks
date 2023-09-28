da58d6e63fa17ed105e11447dc1eeebd9a11fb53
Krishna Kc <IN02107@azentio.com>
2023-09-28 15:39:19 +0530

    First commit

---
+++ src/main/java/com/simply/restservices/SpringbootCodeblocksApplication.java
@@ -0,0 +1,13 @@
+package com.simply.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCodeblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCodeblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simply/restservices/SpringbootCodeblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCodeblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
