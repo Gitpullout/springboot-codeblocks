a792e40a3ac9038ef0daae7a0589fccd31ef841d
Gitpullout <kolaganikrishna@outlook.com>
2023-10-12 16:03:59 +0530

    Second commit - MappingJackson value

--- src/main/java/com/simply/restservices/controllers/UserMappingJacksonController.java
+++ src/main/java/com/simply/restservices/controllers/UserMappingJacksonController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,7 +41,7 @@ public class UserMappingJacksonController {
 	 * ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage()); } }
 	 */
 
-	@GetMapping("/{id}")
+	 @GetMapping("/{id}")  // fields with HashSet
 	public MappingJacksonValue getUserById(@PathVariable("id") @Min(1) Long id) {
 		try {
 
@@ -52,6 +53,24 @@ public class UserMappingJacksonController {
 			fields.add("username");
 			fields.add("ssn");
 			fields.add("orders");
+
+			FilterProvider filterProvider = new SimpleFilterProvider().addFilter("userFilter",
+					SimpleBeanPropertyFilter.filterOutAllExcept(fields));
+			MappingJacksonValue mapper = new MappingJacksonValue(user);
+			mapper.setFilters(filterProvider);
+			return mapper;
+
+		} catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	} 
+	
+	@GetMapping("/params/{id}")  //fields with request params
+	public MappingJacksonValue getUserById2(@PathVariable("id") @Min(1) Long id,@RequestParam Set<String> fields) {
+		try {
+
+			Optional<User> userOptional = userService.getUserById(id);
+			User user = userOptional.get();
 
 			FilterProvider filterProvider = new SimpleFilterProvider().addFilter("userFilter",
 					SimpleBeanPropertyFilter.filterOutAllExcept(fields));
